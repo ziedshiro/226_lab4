43d9190d59999c5b1ae1ebf9e1cf5d6f09379847
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-04 16:23:38 +0700

    6320502991_4_5

--- 6320502991_4_5.c
+++ 6320502991_4_5.c
@@ -3,14 +3,53 @@
 int main()
 {
     int k,i,n,j,r[4],t;;
-    scanf("%d",n);
-    fflush(stdin);
+    scanf("%d",&n);
     char s[n][50];
+    fflush(stdin);
     for(i=0;i<n;i++)
     scanf("%s",s[i]);
     for(i=0;i<4;i++)
         r[i]=0;
     r[0]=1;
+     for(i=0;i<n;i++)
+    {
+        for(j=0;j<strlen(s[i])<j++)
+        {
+        if(s[i][j]=="A")
+        {
+            t=r[1];
+            r[1]=r[2];
+            r[2]=t
+        }
+        else if(s[i][j]=="B")
+        {
+            t=r[0];
+            r[0]=r[3];
+            r[3]=t
+        }
+        else if(s[i][j]=="C")
+        {
+            t=r[0];
+            r[0]=r[3];
+            r[3]=t
+        }
+        else if(s[i][j]=="D")
+        {
+
+        }
+        else if(s[i][j]=="E")
+        {
+
+        }
+        }
+        for(k=0;k<4;k++)
+            if(r==1)
+        {
+            printf("%d\n",k+1);break;
+        }
+
+    }
+
 
 
 
