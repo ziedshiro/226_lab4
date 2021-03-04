4901aa262cfd29555b63b19bf2222bff9a645258
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-04 17:24:54 +0700

    6320502991_4_1

--- 6320502991_4_1.c
+++ 6320502991_4_1.c
@@ -3,8 +3,17 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int i,j,N[n][3];
+    int i,j,k,N[n][3],r[n-2];
+
     for(i=0;i<n;i++)
         for(j=0;j<3;j++)
             scanf("%d",&N[i][j]);
+    for(i=0;i<n-2;i++)
+        for(j=i;j<3;j++)
+            for(k=0;k<3;k++)
+    {
+        if(N[i][0]>0)
+            r[i]
+    }
+
 }
