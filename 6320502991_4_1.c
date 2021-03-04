b815b6fc5ca1f21f1370c0e663edd791239c51fb
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-04 17:43:09 +0700

    6320502991_4_1

--- 6320502991_4_1.c
+++ 6320502991_4_1.c
@@ -3,17 +3,39 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int i,j,k,N[n][3],r[n-2];
-
+    int i,j,k,N[n][3],r[n-2],x=1;
+    for(i=0;i<n-2;i++)
+        r[i]=0;
     for(i=0;i<n;i++)
         for(j=0;j<3;j++)
             scanf("%d",&N[i][j]);
     for(i=0;i<n-2;i++)
-        for(j=i;j<3;j++)
-            for(k=0;k<3;k++)
     {
-        if(N[i][0]>0)
-            r[i]
+        for(j=i;j<i+3;j++)
+    {
+        if(N[j][0]>0)
+        {
+            x=x*N[j][0]*4;
+            r[i]+=x;
+             x=1;
+        }
+        if(N[j][1]>0)
+        {
+            x=x*N[j][1]*2;
+            r[i]+=x;
+             x=1;
+        }
+        if(N[j][2]>0)
+        {
+            x=x*N[j][2]*1;
+            r[i]+=x;
+             x=1;
+        }
     }
+    }
+
+    for(i=0;i<n-2;i++)
+        printf("%d\n",r[i]);
+
 
 }
