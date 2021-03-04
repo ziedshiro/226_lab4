d4766dfd8fcdbd2f6b6e09d5fa6b70b2c7649c35
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-04 16:31:57 +0700

    6320502991_4_5

--- 6320502991_4_5.c
+++ 6320502991_4_5.c
@@ -13,36 +13,46 @@ int main()
     r[0]=1;
      for(i=0;i<n;i++)
     {
-        for(j=0;j<strlen(s[i])<j++)
+        for(j=0;j<strlen(s[i]);j++)
         {
         if(s[i][j]=="A")
         {
             t=r[1];
             r[1]=r[2];
-            r[2]=t
+            r[2]=t;
         }
         else if(s[i][j]=="B")
         {
             t=r[0];
             r[0]=r[3];
-            r[3]=t
+            r[3]=t;
         }
         else if(s[i][j]=="C")
         {
             t=r[0];
             r[0]=r[2];
-            r[2]=t
+            r[2]=t;
             T=r[1];
             r[1]=r[3];
             r[3]=T;
         }
         else if(s[i][j]=="D")
         {
-
+            t=r[0];
+            r[0]=r[1];
+            r[1]=t;
+            T=r[2];
+            r[2]=r[3];
+            r[3]=T;
         }
         else if(s[i][j]=="E")
         {
-
+            t=r[0];
+            r[0]=r[3];
+            r[3]=t;
+            T=r[1];
+            r[1]=r[2];
+            r[2]=T;
         }
         }
         for(k=0;k<4;k++)
