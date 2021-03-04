387dbc7d625f018fdc71ce949da1fcb0e0e012da
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-04 16:01:44 +0700

    6320502991_4_4

--- 6320502991_4_4.c
+++ 6320502991_4_4.c
@@ -3,8 +3,15 @@
 int main()
 {
     char s[1000];
-    scanf("%s",s);
-    int i=0;
-    char t=strtok(s," ");
-
+    scanf("%[^\n]s",s);
+    char* t=strtok(s," "),i=0;
+    while(t!=NULL)
+    {
+        if(i==0)
+            printf("%c",toupper(t[0]));
+        if(i!=0&&strcmp(t,"i")&&strcmp(t,"of")&&strcmp(t,"the")&&strcmp(t,"on")&&strcmp(t,"at")&&strcmp(t,"for")&&strcmp(t,"with")&&strcmp(t,"a")&&strcmp(t,"an")&&strcmp(t,"in")&&strcmp(t,"and"))
+            printf("%c",toupper(t[0]));
+        i++;
+        t=strtok(NULL," ");
+    }
 }
