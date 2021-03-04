add22c4d07437a985dd180bfb14eb775d70db121
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-04 16:22:39 +0700

    6320502991_4_5

---
+++ 6320502991_4_5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int k,i,n,j,r[4],t;;
+    scanf("%d",n);
+    fflush(stdin);
+    char s[n][50];
+    for(i=0;i<n;i++)
+    scanf("%s",s[i]);
+    for(i=0;i<4;i++)
+        r[i]=0;
+    r[0]=1;
+
+
+
+}
